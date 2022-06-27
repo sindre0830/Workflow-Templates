68a64a1e220c88baff3408b3e61cd8d3fdae7854
sindre0830 <sindre0830@gmail.com>
2022-06-27 16:51:19 +0200

    Add CMake example #8

---
+++ examples/cpp/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author Sindre Eiklid
+ * @version 1.0
+ * @date 2022-06-27
+ *
+ * @copyright Copyright (c) 2022 Sindre Eiklid
+ */
+/* external libraries */
+#include <iostream>
+/**
+ * Main program.
+ */
+int main() {
+    std::cout << "Hello, World!\n";
+}
